357a73cd9286255d51b73f4737e6d81e5597b9ba
Sebas2007 <sebastorregroza6@gmail.com>
2026-02-12 15:53:16 -0500

    refactor: reorder imageRoutes import and usage for consistency

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-
 // @/http/index.ts
 import { Elysia } from 'elysia'
 import { CONFIG } from '@/config'
 import { textRoutes } from '@/http/routes/text'
+import { imageRoutes } from '@/http/routes/image'
 import { fromTypes, openapi } from '@elysiajs/openapi'
 import { logger as elysiaLogger } from '@bogeychan/elysia-logger'
 import { loggerConfig } from '@/core/logger'
@@ -16,9 +16,10 @@ app.use(elysiaLogger(loggerConfig))
 app.use(
   openapi({
     references: fromTypes(),
-  }),
+  })
 )
 app.use(textRoutes)
+app.use(imageRoutes)
 
 app.listen(CONFIG.PORT)
 
