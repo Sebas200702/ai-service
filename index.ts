441d19c1a8ef81a30eeb7792ca6796278977f984
Sebas2007 <sebastorregroza6@gmail.com>
2026-02-12 20:00:31 -0500

    refactor: reorganize app initialization and import statements for improved clarity

--- index.ts
+++ index.ts
@@ -1,26 +1,22 @@
-// @/http/index.ts
 import { Elysia } from 'elysia'
-import { CONFIG } from '@/config'
-import { textRoutes } from '@/http/routes/text'
-import { imageRoutes } from '@/http/routes/image'
+import { cors } from '@elysiajs/cors'
 import { fromTypes, openapi } from '@elysiajs/openapi'
 import { logger as elysiaLogger } from '@bogeychan/elysia-logger'
+import { CONFIG } from '@/config'
 import { loggerConfig } from '@/core/logger'
+import { onError } from '@/http/middlewares/error'
+import { textRoutes } from '@/http/routes/text'
+import { imageRoutes } from '@/http/routes/image'
+import { audioRoutes } from '@/http/routes/auidio'
+
 export const app = new Elysia()
-import { cors } from '@elysiajs/cors'
-
-app.use(cors({ origin: '*' }))
-
-app.use(elysiaLogger(loggerConfig))
-
-app.use(
-  openapi({
-    references: fromTypes(),
-  })
-)
-app.use(textRoutes)
-app.use(imageRoutes)
-
-app.listen(CONFIG.PORT)
+  .use(cors({ origin: '*' }))
+  .use(elysiaLogger(loggerConfig))
+  .use(openapi({ references: fromTypes() }))
+  .onError(onError)
+  .use(textRoutes)
+  .use(imageRoutes)
+  .use(audioRoutes)
+  .listen(CONFIG.PORT)
 
 console.log(`🚀 API running on http://localhost:${CONFIG.PORT}`)
