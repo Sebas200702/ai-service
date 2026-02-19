8026ed35463212847ed36202645e761d111e3795
Sebas2007 <sebastorregroza6@gmail.com>
2026-02-19 16:17:05 -0500

    fix: correct import path for generatedImageSchema in image routes

--- src/http/routes/image.ts
+++ src/http/routes/image.ts
@@ -1,7 +1,7 @@
 import { Elysia } from 'elysia'
 import { imageController } from '@/http/controllers/image'
 import { createResponseSchema } from '@/http/openapi/open-api'
-import { generatedImageSchema } from '@/schemas/generated-image'
+import { generatedImageSchema } from '@/schemas/image'
 import { z } from 'zod'
 
 const inputImageSchema = z.object({
